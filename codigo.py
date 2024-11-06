f4621322e3b3ad39fff0fb3236d88f2cf69e2aa9
DemondLive <anaislamaslinda993@gmail.com>
2024-11-06 15:29:03 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -12,6 +12,11 @@ st.write("""
 """)
 st.sidebar.image("Marvel-en-2019-destacada.jpg")
 
+with st.sidebar:
+    st.write("# Cambiar cantidad de Bins")
+    div = st.slider('Número de bins:', 0, 100, 1)
+    st.write("Bins =", div)
+
 # Opciones de colores
 color_options = {
     'Rojo': 'red',
